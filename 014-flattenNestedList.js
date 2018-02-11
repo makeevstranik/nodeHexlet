56a2c44d1e6fed048335f81c5d1bef0ba862afcd
Makeev Evgeny <makeevstranik@gmail.com>
2018-02-11 14:52:38 +0500

    014

---
+++ 014-flattenNestedList.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Makeev Evgeny on 11.02.18.
+ */
+const flatten = arr => arr.reduce((acc, elem) =>
+  elem instanceof Array ? [...acc, ...flatten(elem)] : [...acc, elem], []);
+export default flatten;
+/*
+// HOW TO USE
+const listToCheck = [1, 2, [3, 5], [[4, 3], 2]];
+console.log(flatten(listToCheck));
+*/
