54556e1e990fd5baaefd4b3ce3151493866beccd
Makeev Evgeny <makeevstranik@gmail.com>
2018-02-11 14:31:10 +0500

    013-h

--- 013-findFilesByName.js
+++ 013-findFilesByName.js
@@ -24,14 +24,14 @@ const treeReduce = (fn, tree, acc, index = 1, arr = []) => {
   }
   if (index === arr.length - 1 && tree.type === 'file')console.log('Delete from path --', acc.path.pop()); // псоледний из детей удаляет папку
   if (!tree.children) { // нет
-    console.log('EXIT FROM', tree.type, '***', tree.name);
+  // console.log('EXIT FROM', tree.type, '***', tree.name);
     return acc;
   }
   const fnToReduce = (innerAcc, elem, index, arr) => treeReduce(fn, elem, innerAcc, index, arr);
   acc.path.push(tree.name); //
-  console.log('TREE===>', tree.type, '---', tree.name);
-  console.log('children--->', tree.children);
-  console.log('acc***', acc);
+  // console.log('TREE===>', tree.type, '---', tree.name);
+  //  console.log('children--->', tree.children);
+  // console.log('acc***', acc);
   return tree.children.reduce(fnToReduce, acc); // отдали детей и акк  на редус - пошла папка etc
 };
 const findFilesByName = (tree, strToFind) => {
@@ -39,9 +39,9 @@ const findFilesByName = (tree, strToFind) => {
   return treeReduce(fnPredicate, tree, { accum: [], path: [] }).accum;
 };
 
-console.log(JSON.stringify(tree));
+// console.log(JSON.stringify(tree));
 const result = findFilesByName(tree, 'co');
-console.log('RESULT    ', result);
+// console.log('RESULT    ', result);
 /*
 findFilesByName(tree, 'co');
 // => ['/etc/nginx/nginx.conf', '/etc/consul/config.json']
